f07b2df8029c7e157d5939752ef8ca5d943b872b
Harry Vince <harryavince@gmail.com>
2022-12-01 19:13:46 +0000

    added part 2 of day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -3,11 +3,17 @@ fn main()  {
 
     let lines = input.split("\n\n");
 
-    let linesParsed: Option<u32> = lines
+    let mut linesParsed: Vec<u32> = lines
         .map(|line| line.split("\n")
         .flat_map(|num| num.parse::<u32>())
         .sum::<u32>())
-        .max();
+        .collect();
 
-    println!("{:?}", linesParsed);
+    linesParsed.sort_by(|a, b| b.cmp(a));
+
+    println!("Part 1: {:?}", linesParsed[0]);
+    println!(
+        "Part 2: {:?}",
+        linesParsed.iter().take(3).sum::<u32>()
+    );
 }
