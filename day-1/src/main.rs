5119ffc429765f8035c2ec267e7084dacb6bf16d
Harry Vince <harryavince@gmail.com>
2022-12-01 19:07:30 +0000

    day 1, challenge 1

---
+++ day-1/src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs::File;
+fn main()  {
+    let input = include_str!("./data.txt");
+
+    let lines = input.split("\n\n");
+
+    let linesParsed: Option<u32> = lines
+        .map(|line| line.split("\n")
+        .flat_map(|num| num.parse::<u32>())
+        .sum::<u32>())
+        .max();
+
+    println!("{:?}", linesParsed);
+}
