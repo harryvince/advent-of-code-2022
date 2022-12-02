7a5c8371b9f995ed64f337d6d045e6e8c56c21e9
Harry Vince <harryavince@gmail.com>
2022-12-02 13:40:35 +0000

    part 2 day 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -3,30 +3,59 @@
 // C / Z = Scissors | If won bonus score for chosen item = 3
 // Outcome of round: 0 for Loss, 3 for a draw, 6 if you won
 
+// Part 2
+// X = Lose
+// Y = Draw
+// Z = Win
+
 fn main() {
     let input = std::fs::read_to_string("./input.in").unwrap();
 
     let lines = input.split("\n");
 
-    let mut my_score: Vec<i32> = Vec::new();
+    let mut part1_score: Vec<i32> = Vec::new();
+    let mut part2_score: Vec<i32> = Vec::new();
     
     for line in lines {
         let opp_score = convert_to_number(&line.chars().nth(0).unwrap());
         let my_sc = convert_to_number(&line.chars().nth(2).unwrap());
 
+        // part 1
         if opp_score as f32 / my_sc as f32 != 1.0 {
             if my_sc - 1 == opp_score || my_sc - 1 == opp_score - 3 {
-                my_score.push(6 + my_sc)
+                part1_score.push(6 + my_sc)
             } else {
-                my_score.push(my_sc);
+                part1_score.push(my_sc);
             }
         } else {
-            my_score.push(3 + my_sc);
+            part1_score.push(3 + my_sc);
+        }
+
+        // part 2
+        match my_sc {
+            1=>{
+                if opp_score - 1 == 0 {
+                    part2_score.push(3);
+                } else {
+                    part2_score.push(opp_score-1);
+                }
+            },
+            2=>part2_score.push(opp_score+3),
+            3=>{
+                if opp_score + 1 == 4 {
+                    part2_score.push(1+6); // Rock + Win
+                } else {
+                    part2_score.push((opp_score+1)+6); // Next one up + Win
+                }
+            },
+            __=>println!("Failure.")
         }
     }
     
-    let final_score: i32 = my_score.iter().sum();
-    println!("1st Part: {}", final_score);
+    let part1_final_score: i32 = part1_score.iter().sum();
+    let part2_final_score: i32 = part2_score.iter().sum();
+    println!("1st Part: {}", part1_final_score);
+    println!("2nd Part: {}", part2_final_score);
 }
 
 
