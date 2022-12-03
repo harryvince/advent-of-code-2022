bdd28ddbc96e0010e76636d3a628e9a680f8b42d
Harry Vince <harryavince@gmail.com>
2022-12-03 17:41:40 +0000

    removed clones and borrowed instead

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -7,17 +7,17 @@ fn main() {
 
     for line in lines {
         let compartment_size = line.len() / 2;
-        let compartment_one = line.chars().take(compartment_size);
-        let compartment_two = line.chars().skip(compartment_size);
+        let compartment_one: String = line.chars().take(compartment_size).collect();
+        let compartment_two: String = line.chars().skip(compartment_size).collect();
         
-        'outer: for x in compartment_one.clone() {
-            for y in compartment_two.clone() {
-                if y == x {
-                    if y.is_uppercase() {
-                        collection.push(y as u32 - 38);
+        'outer: for x in 0..compartment_one.len() {
+            for y in 0..compartment_two.len() {
+                if &compartment_two.chars().nth(y).unwrap() == &compartment_one.chars().nth(x).unwrap() {
+                    if compartment_two.chars().nth(y).unwrap().is_uppercase() {
+                        collection.push(compartment_two.chars().nth(y).unwrap() as u32 - 38);
                         break 'outer;
                     } else {
-                        collection.push(y as u32 - 96);
+                        collection.push(compartment_two.chars().nth(y).unwrap() as u32 - 96);
                         break 'outer;
                     }
                 }
